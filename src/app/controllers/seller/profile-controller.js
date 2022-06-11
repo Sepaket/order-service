9e4df9af27322c9cd6c41d2dc93a4229e46cb3f3
ilham pratama <seagleax700@gmail.com>
2022-06-11 13:02:57 +0700

    update response change password in profile

--- src/app/controllers/seller/profile-controller.js
+++ src/app/controllers/seller/profile-controller.js
@@ -1,9 +1,11 @@
 // validator
 const UpdateProfileValidator = require('../../validators/seller/profile/update-profile-validator');
+const ChangePasswordValidator = require('../../validators/seller/profile/change-password-validator');
 
 // responses
 const ProfileResponse = require('../../responses/seller/profile/profile-response');
 const UpdateProfileResponse = require('../../responses/seller/profile/update-profile-response');
+const ChangePasswordResponse = require('../../responses/seller/profile/change-password-response');
 
 module.exports = {
   index: async (request, response, next) => {
@@ -26,6 +28,22 @@ module.exports = {
 
       await new UpdateProfileResponse({ request });
       const result = await new ProfileResponse({ request });
+
+      response.send({
+        code: 200,
+        message: 'OK',
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  changePassword: async (request, response, next) => {
+    try {
+      await ChangePasswordValidator(request.body);
+
+      const result = await new ChangePasswordResponse({ request });
 
       response.send({
         code: 200,
