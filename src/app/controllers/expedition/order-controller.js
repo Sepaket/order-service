72997f51193228f81a5633336eb340fa6f7106ce
ilham pratama <seagleax700@gmail.com>
2022-07-09 17:29:53 +0700

    bulk order -> jne

--- src/app/controllers/expedition/order-controller.js
+++ src/app/controllers/expedition/order-controller.js
@@ -1,10 +1,12 @@
 // validator
 const CommonOrderValidator = require('../../validators/expedition/order/common-order-validator');
+const BulkOrderValidator = require('../../validators/expedition/order/bulk-order-validator');
 
 // responses
 const JneCommonOrderResponse = require('../../responses/expedition/order/common-order/jne-order-response');
 const SicepatCommonOrderResponse = require('../../responses/expedition/order/common-order/sicepat-order-response');
 const NinjaCommonOrderResponse = require('../../responses/expedition/order/common-order/ninja-order-response');
+const JneBulkOrderResponse = require('../../responses/expedition/order/bulk-order/jne-order-response');
 
 module.exports = {
   commonOrder: async (request, response, next) => {
@@ -16,6 +18,24 @@ module.exports = {
       if (body.type === 'JNE') result = await new JneCommonOrderResponse({ request });
       if (body.type === 'SICEPAT') result = await new SicepatCommonOrderResponse({ request });
       if (body.type === 'NINJA') result = await new NinjaCommonOrderResponse({ request });
+
+      response.send({
+        code: 200,
+        message: 'OK',
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  bulkOrder: async (request, response, next) => {
+    try {
+      let result = null;
+      const { body } = request;
+      await BulkOrderValidator(request);
+
+      if (body.type === 'JNE') result = await new JneBulkOrderResponse({ request });
 
       response.send({
         code: 200,
---
+++ src/app/validators/expedition/order/bulk-order-validator.js
@@ -0,0 +1,64 @@
+const joi = require('joi');
+const moment = require('moment');
+const { SellerAddress } = require('../../../models');
+const { serviceCode } = require('../../../../constant/status');
+
+let request = null;
+const isExist = async ({ params, identifier, model }) => new Promise((resolve, reject) => {
+  model.findOne({
+    where: { [`${identifier}`]: params },
+  }).then((result) => {
+    if (!result) reject(new Error(`This ${identifier.split('_').join(' ')} does not exist`));
+    else resolve(true);
+  }).catch((error) => {
+    reject(error.message);
+  });
+});
+
+const serviceCodeValidator = async () => new Promise((resolve, reject) => {
+  const { body } = request;
+  const exist = serviceCode[body.type]?.find((item) => item.code === body.service_code);
+
+  if (exist) resolve(true);
+  else reject(new Error('This service code does not exist'));
+});
+
+/*
+  NOTE
+
+  address length validation from 3pl :
+  - sicepat: 400 char
+  - jne: 80 char
+  - ninja: 100 char
+  - idexpress: 500 char
+*/
+
+const validator = joi.object({
+  type: joi.string().required().valid('JNE', 'SICEPAT', 'NINJA', 'IDEXPRESS'),
+  service_code: joi
+    .string()
+    .required()
+    .external(() => serviceCodeValidator()),
+  should_pickup_with: joi.string().required().valid('MOTOR', 'MOBIL', 'TRUCK'),
+  pickup_date: joi.date().min(moment().format('YYYY-MM-DD')).required(),
+  pickup_time: joi.string().regex(/^([0-9]{2}):([0-9]{2})$/)
+    .message({ 'string.pattern.base': 'pickup time format should HH:mm,' })
+    .required(),
+  seller_location_id: joi
+    .number()
+    .min(1)
+    .required()
+    .external((req) => isExist({ params: req, identifier: 'id', model: SellerAddress })),
+  file: joi.string().required(),
+});
+
+module.exports = (object) => {
+  request = object;
+  return validator.validateAsync(object.body, {
+    errors: {
+      wrap: {
+        label: '',
+      },
+    },
+  });
+};
