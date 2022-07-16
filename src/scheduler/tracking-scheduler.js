c593f86ac33910ead92d87bb98dc776513486899
ilham pratama <seagleax700@gmail.com>
2022-07-16 20:45:30 +0700

    track status sicepat

--- src/scheduler/tracking-scheduler.js
+++ src/scheduler/tracking-scheduler.js
@@ -1,12 +1,15 @@
 const cron = require('node-cron');
 const jne = require('./jne-tracking');
+const sicepat = require('./sicepat-tracking');
 
-const runner = cron.schedule('0 */3 * * *', async () => {
+const runner = cron.schedule('* * * * *', async () => {
+// const runner = cron.schedule('0 */3 * * *', async () => {
   // eslint-disable-next-line no-console
   console.info('tracking scheduler run');
 
   try {
     await jne.tracking();
+    await sicepat.tracking();
   } catch (error) {
     // eslint-disable-next-line no-console
     console.log(error.message);
