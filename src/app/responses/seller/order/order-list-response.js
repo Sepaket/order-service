23f8be112f022b0e87004c1152d38efc24b6446b
ilham pratama <seagleax700@gmail.com>
2022-07-28 00:26:52 +0700

    change sender name into receiver name

--- src/app/responses/seller/order/order-list-response.js
+++ src/app/responses/seller/order/order-list-response.js
@@ -55,7 +55,7 @@ module.exports = class {
               required: true,
               attributes: [
                 ['id', 'receiver_id'],
-                'senderName',
+                'receiverName',
               ],
             },
             {
