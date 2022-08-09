f58a5cb5aa580aa2ced30023b8f0a685284c3b41
Choirool <khoiroelf@gmail.com>
2022-08-09 17:36:12 +0800

    Credit history api admin

---
+++ src/app/responses/admin/balance/history-balance-response.js
@@ -0,0 +1,153 @@
+const moment = require('moment');
+const httpErrors = require('http-errors');
+const { Sequelize } = require('sequelize');
+const { CreditHistory, SellerDetail, Seller } = require('../../../models');
+const snakeCaseConverter = require('../../../../helpers/snakecase-converter');
+
+module.exports = class {
+  constructor({ request }) {
+    this.op = Sequelize.Op;
+    this.request = request;
+    this.credit = CreditHistory;
+    this.seller = Seller;
+    this.sellerDetail = SellerDetail;
+    this.converter = snakeCaseConverter;
+    return this.process();
+  }
+
+  async process() {
+    const limit = 10;
+    const offset = 0;
+    const { query } = this.request;
+    const search = this.querySearch();
+    const total = await this.credit.count();
+
+    const nextPage = (
+      (parseInt(query.page, 10) - parseInt(1, 10)) * parseInt(10, 10)
+    ) || parseInt(offset, 10);
+
+    return new Promise((resolve, reject) => {
+      try {
+        this.credit.findAll({
+          attributes: [
+            ['id', 'credit_id'],
+            'seller_id',
+            'status',
+            'topup',
+            'withdraw',
+            'updatedAt',
+          ],
+          include: [
+            {
+              model: this.sellerDetail,
+              as: 'seller',
+              attributes: [
+                'id',
+                'seller_id',
+                'credit',
+              ],
+              include: [
+                {
+                  model: this.seller,
+                  as: 'seller',
+                  attributes: [
+                    'id',
+                    'name',
+                  ],
+                },
+              ],
+            },
+          ],
+          where: { ...search },
+          order: [['id', 'DESC']],
+          limit: parseInt(query.limit, 10) || parseInt(limit, 10),
+          offset: nextPage,
+        }).then((response) => {
+          const result = this.converter.arrayToSnakeCase(
+            JSON.parse(JSON.stringify(response)),
+          );
+
+          const mapped = result?.map((item) => ({
+            ...item,
+            type: item?.topup ? 'TOPUP' : 'WITHDRAW',
+            description: item?.topup ? 'Topup Saldo' : 'Tarik Saldo',
+          }));
+
+          if (mapped.length > 0) {
+            resolve({
+              data: mapped,
+              meta: {
+                total,
+                total_result: mapped.length,
+                limit: parseInt(query.limit, 10) || limit,
+                page: parseInt(query.page, 10) || (offset + 1),
+              },
+            });
+          } else {
+            reject(httpErrors(404, 'No Data Found', {
+              data: {
+                data: [],
+                meta: {
+                  total,
+                  total_result: 0,
+                  limit: parseInt(query.limit, 10) || limit,
+                  page: parseInt(query.page, 10) || (offset + 1),
+                },
+              },
+            }));
+          }
+        });
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  querySearch() {
+    const { query } = this.request;
+    let filtered = {};
+    if (query?.filter_by === 'DATE_RANGE') {
+      filtered = {
+        updatedAt: {
+          [this.op.between]: [
+            moment(query.date_start).tz('Asia/Jakarta').startOf('day').format(),
+            moment(query.date_end).endOf('day').format(),
+          ],
+        },
+      };
+    }
+
+    if (query.filter_by === 'MONTH') {
+      filtered = {
+        updatedAt: {
+          [this.op.between]: [
+            moment(query.month, 'M').startOf('month').format(),
+            moment(query.month, 'M').endOf('month').format(),
+          ],
+        },
+      };
+    }
+
+    if (query.filter_by === 'YEAR') {
+      filtered = {
+        updatedAt: {
+          [this.op.between]: [
+            moment(query.year, 'YYYY').startOf('year').format(),
+            moment(query.year, 'YYYY').endOf('year').format(),
+          ],
+        },
+      };
+    }
+
+    const condition = {
+      [this.op.or]: {
+        status: { [this.op.eq]: query.status || '' },
+      },
+      [this.op.and]: {
+        ...filtered,
+      },
+    };
+
+    return query?.status || query?.filter_by ? condition : {};
+  }
+};
