73dcf62c24ef67f8701ca639df9bfc36eb54641d
Choirool <khoiroelf@gmail.com>
2022-08-11 10:00:52 +0800

    Order detail, export and print awb

--- src/app/controllers/admin/order-controller.js
+++ src/app/controllers/admin/order-controller.js
@@ -1,13 +1,86 @@
 const BatchValidator = require('../../validators/admin/order/batch-validator');
+const OrderListValidator = require('../../validators/admin/order/list-validator');
+const OrderDetailValidator = require('../../validators/admin/order/detail-validator');
+const ExportValidator = require('../../validators/admin/order/export-validator');
+const PrintValidator = require('../../validators/admin/order/print-validator');
 
 const BatchResponse = require('../../responses/admin/order/order-batch-response');
+const OrderListResponse = require('../../responses/admin/order/order-list-response');
+const OrderDetailResponse = require('../../responses/admin/order/order-detail-response');
+const ExportResponse = require('../../responses/admin/order/order-export-response');
+const PrintResponse = require('../../responses/admin/order/order-print-response');
 
 module.exports = {
-  index: async (request, response, next) => {
+  batch: async (request, response, next) => {
     try {
       await BatchValidator(request.query);
 
       const result = await new BatchResponse({ request });
+
+      response.send({
+        code: 200,
+        message: 'OK',
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  index: async (request, response, next) => {
+    try {
+      await OrderListValidator(request.query);
+
+      const result = await new OrderListResponse({ request });
+
+      response.send({
+        code: 200,
+        message: 'OK',
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  detail: async (request, response, next) => {
+    try {
+      await OrderDetailValidator(request);
+
+      const result = await new OrderDetailResponse({ request });
+
+      response.send({
+        code: 200,
+        message: 'OK',
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  export: async (request, response, next) => {
+    try {
+      let result = null;
+      await ExportValidator(request.body);
+
+      if (request.body.type === 'excel') result = await new ExportResponse({ request });
+
+      response.send({
+        code: 200,
+        message: 'OK',
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  print: async (request, response, next) => {
+    try {
+      await PrintValidator(request.body);
+
+      const result = await new PrintResponse({ request });
 
       response.send({
         code: 200,
