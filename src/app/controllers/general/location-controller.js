416bd20cf70c82db5b80fa75c1a3dff534a8874b
ilham pratama <seagleax700@gmail.com>
2022-08-13 20:19:20 +0700

    add response detail location

--- src/app/controllers/general/location-controller.js
+++ src/app/controllers/general/location-controller.js
@@ -1,8 +1,10 @@
 // validator
 const LocationValidator = require('../../validators/general/location/location-list-validator');
+const LocationDetailValidator = require('../../validators/general/location/location-detail-validator');
 
 // responses
 const LocationListResponse = require('../../responses/general/location/location-list-response');
+const LocationDetailResponse = require('../../responses/general/location/location-detail-response');
 
 module.exports = {
   index: async (request, response, next) => {
@@ -10,6 +12,22 @@ module.exports = {
       await LocationValidator(request.query);
 
       const result = await new LocationListResponse({ request });
+
+      response.send({
+        code: 200,
+        message: 'OK',
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  detail: async (request, response, next) => {
+    try {
+      await LocationDetailValidator(request.body);
+
+      const result = await new LocationDetailResponse({ request });
 
       response.send({
         code: 200,
