cc3d04247f31775863b93f2a453522a495f8cc7d
ilham pratama <seagleax700@gmail.com>
2022-08-21 14:59:33 +0700

    remove constraint unique

---
+++ src/database/migrations/20220821074748-ChangeColumnResiForUnique.js
@@ -0,0 +1,24 @@
+const table = 'orders';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => Promise.all([
+    queryInterface.changeColumn(table, 'resi', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: false,
+    }).then(() => [
+      queryInterface.removeIndex(table, 'resi_order_idx'),
+      queryInterface.removeConstraint(table, 'orders_resi_key'),
+    ]),
+  ]),
+
+  down: async (queryInterface, Sequelize) => Promise.all([
+    queryInterface.changeColumn(table, 'resi', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    }).then(() => [
+      queryInterface.addIndex(table, { fields: ['resi'], name: 'resi_order_idx' }),
+    ]),
+  ]),
+};
