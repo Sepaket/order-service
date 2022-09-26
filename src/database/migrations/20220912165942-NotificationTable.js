124a87ab94bfbda991f1d85eadaf33531e35f8f9
ilham pratama <seagleax700@gmail.com>
2022-09-26 20:03:50 +0700

    add migration for notification

---
+++ src/database/migrations/20220912165942-NotificationTable.js
@@ -0,0 +1,58 @@
+const table = 'notifications';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => Promise.all([
+    queryInterface.createTable(table, {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      message: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      start_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      end_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      is_draft: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    }).then(() => [
+      queryInterface.addIndex(table, { fields: ['title'], name: 'title_notifications_idx' }),
+    ]),
+  ]),
+
+  down: async (queryInterface) => Promise.all([
+    queryInterface.dropTable(table),
+  ]),
+};
