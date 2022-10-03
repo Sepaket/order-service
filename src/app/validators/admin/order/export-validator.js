d4356d9b4afeca4f6850a56c25948e6d6c5adbe7
ilham pratama <seagleax700@gmail.com>
2022-10-03 15:50:36 +0700

    add batch id on export order

--- src/app/validators/admin/order/export-validator.js
+++ src/app/validators/admin/order/export-validator.js
@@ -2,6 +2,7 @@ const joi = require('joi');
 
 const validator = joi.object({
   type: joi.string().required().allow('excel'),
+  batch_id: joi.number().allow('', null),
   date_start: joi.date().required(),
   date_end: joi.date().required(),
 });
