4d7e6eaff14a509d046600a126c6d93d87739e88
Reno Wijoyo <renowijoyo@gmail.com>
2023-05-25 13:53:49 +0700

    ketinggalan index.js

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const bodyParser = require('body-parser');
 
 const errorHandler = require('./src/app/middlewares/errorHandler');
 
-// const batchScheduler = require('./src/scheduler/batch-scheduler');
-// const cleanerNinjaTokenScheduler = require('./src/scheduler/clear-token-scheduler');
-// //
-// const createOrderScheduler = require('./src/scheduler/create-order-scheduler');
-// const ninjaScheduler = require('./src/scheduler/ninja-scheduler')
-// const trackingScheduler = require('./src/scheduler/tracking-scheduler');
+const batchScheduler = require('./src/scheduler/batch-scheduler');
+const cleanerNinjaTokenScheduler = require('./src/scheduler/clear-token-scheduler');
+//
+const createOrderScheduler = require('./src/scheduler/create-order-scheduler');
+const ninjaScheduler = require('./src/scheduler/ninja-scheduler')
+const trackingScheduler = require('./src/scheduler/tracking-scheduler');
 
 // batchScheduler.start();
 // trackingScheduler.start();
